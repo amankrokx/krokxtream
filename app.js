6f8ff8f9275ce13bc798bf13deeba81d255882fc
Aman Kumar <amankumar.spj410@gmail.com>
2021-05-22 19:17:29 +0530

    lol

--- app.js
+++ app.js
@@ -47,8 +47,28 @@ database.ref('command/general/client').on('child_changed', (data) => {
     } else if (data.val() == 'skip' && playing && queuetimer) {
         queuetimer.clear()
         console.log('next song skipped this')
+        nextSong()
     }
 })
+
+let nextSong = () => {
+    database.ref('songs/general/history/'+queue[0].key).update({
+        'status': 'played'
+    })
+    console.log('set status of '+queue[0].key+ ' to played')
+
+    queue.shift()
+    if(queue.length > 0) {
+        console.log('next song')
+        runqueue()}
+    else{
+        console.log('list finished')
+        playing = false
+        database.ref('command/general').set({
+            'play': 'end'
+        }
+    )}
+}
 
 let runqueue = () => {
     playing = true
@@ -58,22 +78,7 @@ let runqueue = () => {
     })
     console.log(queue[0].key)
     queuetimer = new Timer(() => {
-        database.ref('songs/general/history/'+queue[0].key).update({
-            'status': 'played'
-        })
-        console.log('set status of '+queue[0].key+ ' to played')
-    
-        queue.shift()
-        if(queue.length > 0) {
-            console.log('next song')
-            runqueue()}
-        else{
-            console.log('list finished')
-            playing = false
-            database.ref('command/general').set({
-                'play': 'end'
-            }
-        )}
+        nextSong()
     }, queue[0].value.length * 1000)
 }
 
